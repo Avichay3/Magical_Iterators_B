03d5090dd61f05cebdd67e1dda7fac9384c84c4d
Avichay3 <aviaimzina312@gmail.com>
2023-06-15 10:45:52 +0300

    update

--- MyTest.cpp
+++ MyTest.cpp
@@ -55,7 +55,7 @@ TEST_CASE("Test 3 - Iterator SideCross") {
 	mc.addElement(4); //add 4.
 	mc.addElement(2); // add 2.
 
-	MagicalContainer::SideCrossIterator crossIter(mc);
+	MagicalContainer::SideCrossIterator crossIter(mc); // creating instance of side cross iterator.
 
 	CHECK_EQ(*crossIter, 2);//checking if the first element returned by the pointer to iterator is 2.
 	++crossIter; // point to the next.
@@ -67,44 +67,46 @@ TEST_CASE("Test 3 - Iterator SideCross") {
 }
 
 TEST_CASE("Test 4 - Iterator Prime") {
-	MagicalContainer mc;
-	mc.addElement(3);
-	mc.addElement(4);
-	mc.addElement(2);
+	MagicalContainer mc; //creating empty instance of MagicalContainer.
+	mc.addElement(3);//add 3.
+	mc.addElement(4);//add 4.
+	mc.addElement(2);// add 2.
 
-	MagicalContainer::PrimeIterator primesIter(mc);
+	MagicalContainer::PrimeIterator primesIter(mc); // creating instance of prime iterator.
 
 	CHECK_EQ(*primesIter, 2);//checking if the first element returned by the pointer to iterator is 2.
 	++primesIter; // point to the next.
 	CHECK_EQ(*primesIter, 3);
 	++primesIter; // point to the next.
-	CHECK_EQ(primesIter, primesIter.end());
+	CHECK_EQ(primesIter, primesIter.end()); //check if iterator has reached the end.
 }
 
 TEST_CASE("Test 5 - Check iterators comparison") {
-	MagicalContainer mc;
-	mc.addElement(3);
-	mc.addElement(4);
-	mc.addElement(2);
+	MagicalContainer mc; //creating empty instance of MagicalContainer.
+	mc.addElement(3);//add 3.
+	mc.addElement(4);//add 4.
+	mc.addElement(2);// add 2.
 
-	MagicalContainer::AscendingIterator ascIter1(mc);
-	MagicalContainer::AscendingIterator ascIter2(mc);
-	MagicalContainer::SideCrossIterator crossIter1(mc);
-	MagicalContainer::SideCrossIterator crossIter2(mc);
-	MagicalContainer::PrimeIterator primesIter1(mc);
-	MagicalContainer::PrimeIterator primesIter2(mc);
+	MagicalContainer::AscendingIterator ascIter1(mc);//creating ascending iterator and initialize with mc
+	MagicalContainer::AscendingIterator ascIter2(mc); //creating ascending iterator and initialize with mc
+	MagicalContainer::SideCrossIterator crossIter1(mc); //creating sideCross iterator and initialize with mc
+	MagicalContainer::SideCrossIterator crossIter2(mc); //creating sideCross iterator and initialize with mc
+	MagicalContainer::PrimeIterator primesIter1(mc); //creating prime iterator and initialize with mc
+	MagicalContainer::PrimeIterator primesIter2(mc); //creating prime iterator and initialize with mc
 
-	CHECK_EQ(ascIter1, ascIter2);
-	CHECK_EQ(crossIter1, crossIter2);
-	CHECK_EQ(primesIter1, primesIter2);
+	CHECK_EQ(ascIter1, ascIter2); //check if both  instances are initially pointing to the same position in the container.
+	CHECK_EQ(crossIter1, crossIter2);  //check if both  instances are initially pointing to the same position in the container.
+	CHECK_EQ(primesIter1, primesIter2);  //check if both  instances are initially pointing to the same position in the container.
 
-	++ascIter1;
-	++crossIter1;
-	++primesIter1;
+	++ascIter1; //increment to the next element in ascending.
+	++crossIter1; //increment to the next element in sideCross.
+	++primesIter1; //increment to the next element in prime.
 	
-	CHECK_NE(ascIter1, ascIter2);
-	CHECK_GT(ascIter1, ascIter2);
-	CHECK_LT(ascIter2, ascIter1);
+	CHECK_NE(ascIter1, ascIter2); //check if not equal
+	CHECK_GT(ascIter1, ascIter2); //check if ascIter1 greater than ascIter2
+	CHECK_LT(ascIter2, ascIter1); //check if ascIter2 lower than ascIter1
+
+    /*same thing for the rest lines*/
 
 	CHECK_NE(crossIter1, crossIter2);
 	CHECK_GT(crossIter1, crossIter2);
